d9881db8017f5e75a0895354e46a657a1dc6fcb0
HardToRaid <coscupe78@gmail.com>
2024-02-02 23:01:34 -0500

    Creacion del token con jwt y bcrypt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,30 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcrypt'
+import { createAccesToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body
 
   try {
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
       email,
       username,
-      password
+      password: passwordHash
     })
-    await newUser.save()
-    // console.log(newUser)
+    const userSaved = await newUser.save()
+    const token = await createAccesToken({ id: userSaved._id })
+    res.cookie('token', token)
+    res.json({
+      id: userSaved.id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updateAt: userSaved.updatedAt
+    })
   } catch (error) {
-    console.log(error)
+    res.status(500).json({ message: error.message })
   }
-
-  res.send('registrando')
 }
 export const login = (req, res) => res.send('login')
