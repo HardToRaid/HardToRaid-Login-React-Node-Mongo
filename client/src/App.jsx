a54cdc668f6f5d7aecf50ef85523881f6d03f48b
HardToRaid <coscupe78@gmail.com>
2024-03-01 23:16:42 -0500

    Registro de usuarios con el front(react)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import RegisterPage from './pages/RegisterPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 
 function App() {
 
   return (
-    <h1>Hola Mundo</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/task' element={<h1>Task Page</h1>} />
+        <Route path='/add-task' element={<h1>New Task</h1>} />
+        <Route path='/task/:id' element={<h1>Update Task</h1>} />
+        <Route path='/profile' element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
